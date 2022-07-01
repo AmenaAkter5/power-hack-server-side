f7eba1c44fb6f1e040d2844519f318d1e22b042a
unknown <latapata1952@gmail.com>
2022-07-01 12:52:08 +0600

    authentication handle

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-// import mongo 
-// const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
+
 
 
 
@@ -20,6 +22,109 @@ app.use(express.json());
 
 
 // connect with mongo database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eaoeh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// set connection function
+async function run() {
+    try {
+        await client.connect();
+
+
+        // create user collection
+        const userCollection = client.db("power_hack").collection("users")
+
+
+        // user registration api
+
+        app.post('/api/registration', async (req, res) => {
+            console.log(req.body);
+            const { name, email, password } = req.body;
+
+            if (!name || typeof name !== 'string') {
+                return res.json({ status: 'error', error: 'Invalid username' })
+            }
+
+            if (!password || typeof password !== 'string') {
+                return res.json({ status: 'error', error: 'Invalid password' })
+            }
+
+            if (password.length < 5) {
+                return res.json({
+                    status: 'error',
+                    error: 'Password too small. Should be atleast 6 characters'
+                })
+            }
+
+            const encryptedPassword = await bcrypt.hash(password, 10)
+
+            try {
+                const response = await userCollection.insertOne({
+                    name,
+                    email,
+                    password: encryptedPassword
+                })
+                console.log('User created successfully: ', response)
+            } catch (error) {
+                if (error.code === 11000) {
+                    // duplicate key
+                    return res.json({ status: 'error', error: 'Username already in use' })
+                }
+                throw error
+            }
+
+            res.json({ status: 'ok' })
+        })
+
+
+        // user login api
+
+        app.post('/api/login', async (req, res) => {
+            // console.log(req.body);
+            const { email, password } = req.body;
+
+            const user = await userCollection.findOne({ email });
+
+            if (!user) {
+                return res.json({ status: 'error', error: 'Invalid email/password' })
+            }
+
+            if (await bcrypt.compare(password, user.password)) {
+                // the username, password combination is successful
+
+                // token issue
+                const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+
+                // res.send({ result, token });
+
+                /* const token = jwt.sign(
+                    {
+                        id: user._id,
+                        username: user.username
+                    },
+                    JWT_SECRET
+                ) */
+
+                return res.json({ status: 'ok', data: token })
+            }
+
+            res.json({ status: 'error', error: 'Invalid username/password' })
+        })
+
+
+    }
+
+    finally {
+        // client.close();
+    }
+}
+
+
+run().catch(console.dir);
+
 
 
 // Make API : check server root
